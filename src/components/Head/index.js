d3a62d1668b48389a5384b14dd812c659401121c
shiva <smpgr2351@gmail.com>
2024-04-04 17:02:09 +0530

    7 commit

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -8,9 +8,9 @@ class Head extends Component{
     render(){
         console.log("head")
         return <nav className="head">
-                <ul>
+                <ul className="head-ul">
                     <li>
-                    <img alt="logo" src="https://res.cloudinary.com/delrn2vxa/image/upload/v1711875446/Group_7146_k9oli0.png" />
+                    <img alt="logo" className="head-logo" src="https://res.cloudinary.com/delrn2vxa/image/upload/v1711875446/Group_7146_k9oli0.png" />
 
                     </li>
                     <li>
